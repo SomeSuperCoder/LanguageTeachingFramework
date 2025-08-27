ccca0287dc7321f46447d4f616a17670822e38e8
SomeSuperCoder <ithelper9@gmail.com>
2025-08-27 15:01:24 +0300

    Welcome

---
+++ src/Welcome.tsx
@@ -0,0 +1,25 @@
+export default function Welcome() {
+  return (
+    <main className="h-screen flex justify-center align-center">
+      <div className="flex flex-col justify-center gap-10">
+        <h1 className="text-7xl">
+          Make language <br /> teaching{" "}
+          <span className="text-orange-500 underline text-shadow-xs text-shadow-orange-200">
+            easier!
+          </span>
+        </h1>
+        <div className="flex justify-around text-lg">
+          <button className="bg-purple-700 p-3 rounded-md hover:bg-purple-600">
+            Why us?
+          </button>
+          <button className="bg-blue-700 p-3 rounded-md	hover:bg-blue-600 ">
+            Get started &rarr;
+          </button>
+        </div>
+      </div>
+      <button className="fixed bg-purple-600 bottom-4 text-xl rounded-full h-10 w-10 animate-bounce hover:bg-purple-500">
+        &darr;
+      </button>
+    </main>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Welcome from "./Welcome.tsx";
+
+const router = createBrowserRouter([{ path: "/", element: <Welcome /> }]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
